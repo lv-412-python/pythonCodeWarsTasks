86ae1d833af615aa07cefb69075ae392c2e0ab37
Liubomyr Halamaha <galamagal86@gmail.com>
2019-05-17 19:14:04 +0300

    added skeleton
    add example
    add file requirements.txt
    add base config for travis.

---
+++ main.py
@@ -0,0 +1,12 @@
+"""ToDo."""
+from tasks.kyu8 import example
+
+
+def run_example():
+    """ToDo"""
+    first = input("a: ")
+    second = input("b: ")
+    print(example(first, second))
+
+if __name__ == '__main__':
+    run_example()
---
+++ tasks/kyu8.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""Example Google style docstrings.
+   http://google.github.io/styleguide/pyguide.html
+
+"""
+
+
+def example(first, second):
+    """calculate sum a, b.
+    Args:
+        first (int): The first parameter.
+        second (int): The second parameter.
+
+    Returns:
+        int: The return value.
+    """
+    return first + second
