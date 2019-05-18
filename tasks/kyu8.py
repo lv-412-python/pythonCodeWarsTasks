71ee0d631d329de4b28cb0e635440313201d26cc
Olha <khomyn2314@gmail.com>
2019-05-18 10:24:26 +0300

    Added function divisible_by

--- tasks/kyu8.py
+++ tasks/kyu8.py
@@ -15,3 +15,19 @@ def example(first, second):
         int: The return value.
     """
     return first + second
+
+
+def divisible_by(array, divisor):
+    """
+        Returns a list of numbers which are divisible by given number
+        Args:
+            array (list): The first parameter
+            divisor (int): The second parameter
+        Returns:
+            list: The return value
+    """
+    return_list = list()
+    for i in array:
+        if i % divisor == 0:
+            return_list.append(i)
+    return return_list
