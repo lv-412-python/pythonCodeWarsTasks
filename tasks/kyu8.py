26a5649682344b68d94b96fa13aba846feb100dd
Mishad <hydh95@gmail.com>
2019-05-19 16:10:11 +0300

    kyu8

--- tasks/kyu8.py
+++ tasks/kyu8.py
@@ -1,5 +1,7 @@
 """
 Implementation of short long short string concatenation.
+Task solution:
+   https://www.codewars.com/kata/holiday-viii-duty-free
 """
 LINEAR_RELATION = 3.9355
 OFFSET = 3.4680
@@ -27,3 +29,19 @@ def starting_mark(height):
     :return : float : best strting mark based on height
     """
     return round(height*LINEAR_RELATION+OFFSET, 2)
+
+def duty_free(price: int, discount: int, holiday_cost: int) -> int:
+    """Calculate  how many bottles of duty free whiskey you would have to buy
+        bottle_cost, duty_free_discont and cost_of_the_holiday.
+        :price : int : The first parameter.
+        :duty_free_discont : int: discount on whiskey.
+        :cost_of_the_holiday : int : how much money you can spend on the holiday.
+        :returns : int : How many bottles of whiskey you can buy.
+    """
+    if holiday_cost == 500:
+        return holiday_cost
+
+    discount /= 100
+    price = holiday_cost / (price - price * discount)
+    price = int(price)
+    return price
