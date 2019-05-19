6e94d42981941535b9d75d195b178db69fea0e7e
Olha <khomyn2314@gmail.com>
2019-05-19 22:22:37 +0300

    Added kyu8 task

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 """ToDo."""
 from tasks.kyu8 import example
+from tasks.kyu8 import Point, Circle, circle_area
 
 
 def run_example():
     """ToDo"""
-    first = input("a: ")
-    second = input("b: ")
+    first = int(input("a: "))
+    second = int(input("b: "))
     print(example(first, second))
+
+
+def run_circle_area():
+    """Prints the are of a circle"""
+    x_point = int(input('Input x: '))
+    y_point = int(input('Input y: '))
+    point = Point(x_point, y_point)
+    radius = int(input('Input radius: '))
+    circle = Circle(point, radius)
+    area = circle_area(circle)
+    print("The area of circle: {}".format(area))
+
 
 if __name__ == '__main__':
     run_example()
+    run_circle_area()
--- tasks/kyu8.py
+++ tasks/kyu8.py
@@ -3,6 +3,7 @@
    http://google.github.io/styleguide/pyguide.html
 
 """
+import math
 
 
 def example(first, second):
@@ -15,3 +16,73 @@ def example(first, second):
         int: The return value.
     """
     return first + second
+
+
+class Circle(object):
+    """
+    A class used to represent a Circle
+    """
+
+    def __init__(self, point, radius):
+        """
+        Args:
+            point (Point): The first parameter
+            radius (float): The second parameter
+        """
+        self._point = point
+        self._radius = radius
+
+    @property
+    def point(self):
+        """
+        returns: str
+        """
+        return self._point
+
+    @property
+    def radius(self):
+        """
+        returns: float or int
+        """
+        return self._radius
+
+
+class Point(object):
+    """
+    A class used to represent a Point
+    """
+
+    def __init__(self, x_point, y_point):
+        """
+        Args:
+            x_point (int or float): The first parameter
+            y_point (int or float): The second parameter
+        """
+        self._x = x_point
+        self._y = y_point
+
+    @property
+    def x_point(self):
+        """
+        returns: int or float
+        """
+        return self._x
+
+    @property
+    def y_point(self):
+        """
+        returns: int or float
+        """
+        return self._y
+
+
+def circle_area(circle):
+    """"
+    Calculates the area of a circle
+
+    Args:
+        circle (Circle)
+    Returns:
+        float: The return value
+    """
+    return math.pi * circle.radius * circle.radius
