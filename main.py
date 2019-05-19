254f534a4dc17c2f1904f710036decfa16a448b3
Olya Petryshyn <olya.petryshyn@gmail.com>
2019-05-19 16:53:13 +0300

    added task kyu5 - fibonacci product and modified main,py.

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 """ToDo."""
-from tasks.kyu8 import example
+from tasks.kyu5 import fib_product
 
 
-def run_example():
-    """ToDo"""
-    first = input("a: ")
-    second = input("b: ")
-    print(example(first, second))
+def run_fib_product():
+    """
+    Runs fib_product function.
+    """
+    try:
+        prod = int(input("Enter product of 2 numbers: "))
+        print(fib_product(prod))
+    except ValueError:
+        print("Wrong input!")
+
 
 if __name__ == '__main__':
-    run_example()
+    run_fib_product()
