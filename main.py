31828817b9007c8dad6ffcd37d0d8e5846fd629b
Olya Petryshyn <olya.petryshyn@gmail.com>
2019-05-19 19:54:00 +0300

    added first task 8kyu and modified main.py.

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 """ToDo."""
-from tasks.kyu8 import example
+from tasks.kyu8 import short_long_short
 
 
-def run_example():
-    """ToDo"""
-    first = input("a: ")
-    second = input("b: ")
-    print(example(first, second))
+def run_sls():
+    """
+    Runs short_long_short function.
+    """
+    first_str = input("Enter string: ")
+    second_str = input("Enter another string: ")
+    while len(first_str) == len(second_str):
+        print("String can't be the same length.")
+        first_str = input("Enter string: ")
+        second_str = input("Enter another string: ")
+    print(short_long_short(first_str, second_str))
+
 
 if __name__ == '__main__':
-    run_example()
+    run_sls()
