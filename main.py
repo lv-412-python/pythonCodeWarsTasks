9efa61543ede418279441bc04d7301c47e5f9cce
Taras Konchak <taras.konchak1@gmail.com>
2019-05-20 21:56:31 +0300

    fix

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 """ToDo."""
-from tasks.kyu6 import nba_cup, R
+from tasks.kyu5 import nba_cup, R
 
 
 def Run():
     """ToDo"""
     team = input('Enter the name of the team:   ')
     print(nba_cup(R, team))
+
+
+
 
 if __name__ == '__main__':
     Run()
