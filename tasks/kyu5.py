a2a2420eb1a1745e703db4abc1a2226a30145f29
Olha <8unicorn89@gmail.com>
2019-05-22 21:18:28 +0300

    Changed docstring

--- tasks/kyu5.py
+++ tasks/kyu5.py
@@ -9,10 +9,8 @@ def solve(limit):
     """
     Calculates x out of sequence 'x + 2x**2 + 3x**3 + .. + nx**n'
 
-    Args:
-        limit (float or int): The limit of the sequence
-    Returns:
-        float: The return value
+    :param limit: float : The limit of the sequence.
+    :return: float : argument of the sequence.
     """
     discriminant = math.pow((1 + 2 * limit), 2) - 4 * limit * limit
     argument = (- (1 + 2 * limit) + math.sqrt(discriminant)) / (-2 * limit)
