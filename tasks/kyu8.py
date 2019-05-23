35174c0a940a02f766f675abdfb6db99dac76cbc
LSikyr <sikyrynskyi@gmail.com>
2019-05-23 11:44:32 +0300

    kyu8 - keep-hydrated

--- tasks/kyu8.py
+++ tasks/kyu8.py
@@ -10,3 +10,11 @@ def short_long_short(first_str, second_str):
     if len(first_str) > len(second_str):
         return ''.join(second_str + first_str + second_str)
     return ''.join(first_str + second_str + first_str)
+
+def litres(time):
+    """
+    calculate needed amount of water
+    param time: float : time of run
+    return : int : needed amount of water based on time
+    """
+    return int(time/2)
